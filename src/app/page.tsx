a40f92e77e5d384e8fbe6688e76b315fd8669215
eyebrowartistmimi-rgb <eyebrowartist.mimi@gmail.com>
2026-02-07 16:37:26 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
 'use client'
 
 import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
 
 export default function Home() {
-    const router = useRouter()
+      useEffect(() => {
+              window.location.href = 'https://bibimake-calendar.vercel.app/'
+      }, [])
 
-  useEffect(() => {
-        window.location.href = 'https://bibimake-calendar.vercel.app/'
-  }, [])
-
-  return (
-        <div className="min-h-screen flex items-center justify-center">
-              <div className="text-xl">リダイレクト中...</div>div>
-        </div>div>
-      )
-}</div>
+  return null
+}
